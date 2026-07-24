0753f642f7b6a0025550f3fea7b7009a4b6c94cd
Digitador <transformacaodigital@luariembalagens.com.br>
2026-07-24 16:00:31 -0300

    ajustes

--- servicos/google_sheet.py
+++ servicos/google_sheet.py
@@ -1,7 +1,16 @@
+# Essa biblioteca é como openpyxl mas mais pesado pois analis a planilha no navegador
+import pandas as pd 
+
 def converter_google_sheets(link):
     if "docs.google.com/spreadsheets" not in link:
         raise Exception("O link inserido não é um Google-Sheets")
 
+# Substitui o final do link e o transforma em um arquivo baixável no navegador
     link_csv = link.replace("/edit?", "/export?format=csv&")
+
+# Pega o arquivo no navegador (internet) ao acessá-lo, baixa e cria o dataframe
+    df=pd.read_csv(link_csv)
+
+    print(df)
 
     return link_csv
